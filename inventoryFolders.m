6918dceeb71dba70ca63c46b617e5b2cca59080e
Geoffrey Jones <jonesg5@rpi.edu>
2015-12-15 10:38:52 -0500

    Expanded inventoryFolders

--- inventoryFolders.m
+++ inventoryFolders.m
@@ -22,9 +22,10 @@ downloaded_files = fullfile(subject,'downloaded_files');
 diaries = fullfile(subject,'diaries');
 best_diary = fullfile(subject,'best_diary');
 diagnostic_reports = fullfile(subject,'diagnostic_reports');
+do_NOT_use = fullfile(subject,'do_NOT_use');
 
 paths = table(subject, best_download, marked_download, ...
-    downloaded_files, diaries, best_diary, diagnostic_reports);
+    downloaded_files, diaries, best_diary, diagnostic_reports, do_NOT_use);
 
 nSubject = numel(subject);
 folder_name = {listingSubjectDir.name}';
@@ -32,6 +33,7 @@ best_diary_exists = false(nSubject,1);
 downloaded_files_exists = false(nSubject,1);
 best_download_exists = false(nSubject,1);
 marked_download_exists = false(nSubject,1);
+do_NOT_use_exists = false(nSubject,1);
 
 for iSubject = 1:nSubject
     thesePaths = paths(iSubject,:);
@@ -51,15 +53,19 @@ for iSubject = 1:nSubject
     % Check for marked download
     markedDownloadListing = dir([thesePaths.marked_download{1},filesep,'*.cdf']);
     marked_download_exists(iSubject) = ~isempty(markedDownloadListing);
+    
+    % Check for do NOT USE
+    do_NOT_use_exists(iSubject) = isdir(thesePaths.do_NOT_use{1});
 end
 
 folder_inventory = table(folder_name, downloaded_files_exists, ...
-    best_diary_exists, best_download_exists, marked_download_exists);
+    best_diary_exists, best_download_exists, marked_download_exists, ...
+    do_NOT_use_exists);
 
 timestamp = datestr(now,'yyyy-mm-dd_HHMM');
 excelPath = fullfile(rootDir,'weekly_reports',['folder_inventory_',timestamp,'.xlsx']);
 writetable(folder_inventory,excelPath);
-winopen(excelPath);
+%winopen(excelPath);
 
 end
 
